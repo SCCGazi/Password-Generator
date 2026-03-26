2ad0361250d2225da89cdc0aca3fc8dfcd56d3da
SCCGazi <gxskbyl@gmail.com>
2026-03-26 12:04:20 -0700

    Refactor password generator script for clarity

--- bin/passwordgenerator.sh
+++ bin/passwordgenerator.sh
@@ -1,13 +1,65 @@
 #!/bin/bash
-source /workspaces/Password-Generator/lib/tools.sh
-echo "-----PASSWORD GENERATOR-----"
-echo "----------------------------"
+source tools.sh
+echo "------------------------PASSWORD GENERATOR-------------------"
+echo "----------------The app generates random passwords-----------"
 
-# Ask for digits and number of random numbers
-digits=$(getNumber "Enter amount of digits: ")
-random=$(getNumber "How many random numbers to generate: ")
+# Character sets
+LOWER="abcdefghijklmnopqrstuvwxyz"
+UPPER="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+DIGITS="0123456789"
+SPECIAL="!@#$%^&*()-_=+[]{};:,.<>/?"
+ALL="$LOWER$UPPER$DIGITS$SPECIAL"
 
-while true; do
+    char_password()
+    {
+     # Ask for min and max length
+        VALID=false
+        num_passwords=$(getNumber "How many passwords to generate? ")
+    while [ "$VALID" = false ]; do
+        min_len=$(getNumber "Minimum password length: ")
+        max_len=$(getNumber "Maximum password length: ")
+
+        if [[ $min_len -gt $max_len ]]; then
+            echo "ERROR: Minimum lenght must be less than Maximum lenght" >&2
+        else
+            VALID=true
+        fi
+
+    done
+
+#STOP HERE FOR ...
+     # Generate passwords
+    for ((i=1; i<=num_passwords; i++)); do
+        length=$(( RANDOM % (max_len - min_len + 1) + min_len ))
+        password="" 
+       
+        password+=${LOWER:$((RANDOM % ${#LOWER})):1} 
+        password+=${UPPER:$((RANDOM % ${#UPPER})):1}
+        password+=${DIGITS:$((RANDOM % ${#DIGITS})):1}
+        password+=${SPECIAL:$((RANDOM % ${#SPECIAL})):1}
+
+        # Fill the rest with random characters
+        for ((j=5; j<=length; j++)); do
+            password+=${ALL:$((RANDOM % ${#ALL})):1}
+        done
+
+        password=$(echo "$password" | fold -w1 | shuf | tr -d '\n')
+        passwords+=("$password") 
+    done
+
+    # Display passwords
+    echo "Generated passwords:"
+    for ((i=0; i<${#passwords[@]}; i++)); do
+        echo "$((i+1)). ${passwords[i]}"
+    done
+    generate_again
+    }
+
+    password()
+    {
+    # Ask for digits and number of random numbers
+    digits=$(getNumber "Enter amount of digits: ")
+    random=$(getNumber "How many random numbers to generate: ")
     # Input min and max
     VALID=false
     while [ "$VALID" = false ]; do
@@ -56,7 +108,11 @@ while true; do
     for ((i=0; i<${#numbers[@]}; i++)); do
         echo "$((i+1)). ${numbers[i]}"
     done
+    generate_again
+    }
 
+    generate_again()
+    {
     # Ask user if they want to generate again
     while true; do
         echo -n "Do you want to generate another set? (y/n): "
@@ -72,4 +128,26 @@ while true; do
             echo "Invalid input, please press y or n"
         fi
     done
+    }
+
+while true; do
+ 
+      # Ask user if they want to generate again
+    while true; do
+        echo "-----------------------------------------------------------------"
+        echo "Do you want to generate random passwords with characters?"
+        echo -ne "Press y for yeas and n for no  (y/n):\n"
+        echo "-----------------------------------------------------------------"
+        read -n1 answer 
+        echo  
+
+        if [[ $answer == [Yy] ]]; then
+               char_password
+        elif [[ $answer == [Nn] ]]; then
+               password
+        else
+            echo "Invalid input, please press y or n"
+        fi
+    done
+
 done
